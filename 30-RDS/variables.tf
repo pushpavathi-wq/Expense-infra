3d81d8df57c3e40541d2deaa4be7f6c7943098c7
pushpavathi-wq <valasapushpa13@gmail.com>
2026-05-29 10:49:53 +0530

    commit RDS

---
+++ 30-RDS/variables.tf
@@ -0,0 +1,16 @@
+variable "project_name" {
+  
+}
+
+variable "environment" { 
+  
+}
+
+
+variable "zone_id" {
+    default = "Z09655703QCDO6BLZJFRA"
+}
+
+variable "domain_name" {
+    default = "daws82s.fun"
+}
